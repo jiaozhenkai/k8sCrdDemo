6b06c1cfedf6caded6f1d87213a718cc42f292d5
jiaozhenkai <jiaozhenkai96@gmail.com>
2020-07-08 21:57:00 +0800

    add

--- k8s-gen.sh
+++ k8s-gen.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 
 # 代码生成的工作目录，也就是我们的项目路径
 ROOT_PACKAGE="github.com/jiaozhenkai/k8scrddemo"
+# ROOT_PACKAGE="/home/jiaozhenkai/Desktop/k8sCrdDemo"
 # API Group
 CUSTOM_RESOURCE_NAME="hero"
 # API Version
 CUSTOM_RESOURCE_VERSION="v1"
 
+GO111MODULE=off
+
 # 安装k8s.io/code-generator
-go get -u k8s.io/code-generator/...
+# go get -u k8s.io/code-generator/...
 cd $GOPATH/src/k8s.io/code-generator
+
 
 # 执行代码自动生成，其中pkg/client是生成目标目录，pkg/apis是类型定义目录
 ./generate-groups.sh all "$ROOT_PACKAGE/pkg/client" "$ROOT_PACKAGE/pkg/apis" "$CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
